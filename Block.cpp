a33c36227ea9a57b342bcb0fae838e16f1e6f968
samzhuwj <samzhuwj@gmail.com>
2018-09-27 22:20:14 +0800

    MineBlock

--- Block.cpp
+++ Block.cpp
@@ -7,3 +7,20 @@ Block::Block(uint32_t nIndexIn, const string &sDataIn) : _nIndex(nIndexIn), _sDa
   _tTime = time(nullptr);
   sHash = _CalculateHash();
 }
+
+void Block::MineBlock(uint32_t nDifficulty)
+{
+  char cstr[nDifficulty + 1];
+
+  for (uint32_t i = 0; i < nDifficulty; ++i) cstr[i] = '0';
+
+  cstr[nDifficulty] = '\0';
+  string str(cstr);
+
+  do {
+    _nNonce++;
+    _sHash = _CalculateHash();
+  } while (_sHash.substr(0, nDifficulty) != str);
+
+  cout << "Block mined: " << _sHash << endl;
+}
