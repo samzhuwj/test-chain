aabae6bdd228646ebb2cb2e2e80b047ae945d0a9
samzhuwj <samzhuwj@gmail.com>
2018-09-27 20:15:39 +0800

    include libraries and header file

---
+++ Blockchain.h
@@ -0,0 +1,10 @@
+#ifndef TESTCHAIN_BLOCKCHAIN_H
+#define TESTCHAIN_BLOCKCHAIN_H
+
+#include <cstdint>
+#include <vector>
+#include "Block.h"
+
+using namespace std;
+
+#endif //TESTCHAIN_BLOCKCHAIN_H
