adf4297f30a0d3512aca2d591f2c38f75bbd559d
samzhuwj <samzhuwj@gmail.com>
2018-09-27 22:58:55 +0800

    adding a block to the blockchain

--- Blockchain.cpp
+++ Blockchain.cpp
@@ -5,3 +5,10 @@ Blockchain::Blockchain()
   _vChain.emplace_back(Block(0, "Genesis Block"));
   _nDifficulty = 6;
 }
+
+void Blockchain::AddBlock(Block bNew)
+{
+  bNew.sPrevHash = _GetLastBlock().sHash;
+  bNew.MineBlock(_nDifficulty);
+  _vChain.push_back(bNew);
+}
